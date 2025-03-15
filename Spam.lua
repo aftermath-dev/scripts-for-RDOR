17201f1bf5d24a803a6777f82a843bf1aea9fb82
Aftr <evgenijzulin629@gmail.com>
2025-03-15 21:36:54 +0300

    Update Spam.lua

--- Spam.lua
+++ Spam.lua
@@ -101,46 +101,6 @@ TextButton1.TextSize = 14.000
 TextButton1.TextWrapped = true
 TextButton1.Visible = false
 
-local UserInputService = game:GetService("UserInputService")
-
-local gui = ScreenGui
-
-local dragging
-local dragInput
-local dragStart
-local startPos
-
-local function update(input)
-	local delta = input.Position - dragStart
-	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-end
-
-gui.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-		dragging = true
-		dragStart = input.Position
-		startPos = gui.Position
-
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragging = false
-			end
-		end)
-	end
-end)
-
-gui.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		dragInput = input
-	end
-end)
-
-UserInputService.InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		update(input)
-	end
-end)
-
 spam = false
 local function send(message)
 	if game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents") then
